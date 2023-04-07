e71f048525549a132b0eae8cc9a967b56c57b609
Michal Kwiecien <michal.t.kwiecien@gmail.com>
2023-04-07 14:55:51 +0200

    Initial Commit

---
+++ Folderify/FolderifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FolderifyApp.swift
+//  Folderify
+//
+//  Created by Michał Kwiecień on 07/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct FolderifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
